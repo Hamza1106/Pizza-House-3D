fc373846a85eeadf9c548b3ab7357806e32d4d94
Hamza1106 <hamzaqureshi0128@gmail.com>
2026-06-24 09:30:30 +0500

    Updated index.css

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -6,10 +6,12 @@ export function useLenis() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.0,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      touchMultiplier: 2,
+      wheelMultiplier: 1,
+      touchMultiplier: 1.5,
+      infinite: false,
     });
     lenisRef.current = lenis;
 
